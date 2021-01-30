8dd23f39c7cfb543b403fccc9e3b70c2d7329311
Stony <stony.lohr@gmail.com>
2021-01-30 10:32:46 -0800

    difference adjustments and tests

--- src/util.rs
+++ src/util.rs
@@ -14,20 +14,74 @@ pub fn calc_diff_abs(x: f64, y: f64) -> (f64, bool) {
     (diff_abs, sign_change)
 }
 
+// Return the relative difference between two values.
+// If both values are nan or same-sign infinite, consider the difference to be 0.
+pub fn calc_diff_rel(x: f64, y: f64) -> (f64, bool) {
+    let (mut diff, sign_change) = calc_diff_abs(x, y);
+    if diff != 0.0 { // and implicitly not nan
+        diff *= 2.0 / (x.abs() + y.abs());
+    }
+    (diff, sign_change)
+}
+
 // Return the lesser of the absolute and relative difference between two values.
 // If both values are nan or same-sign infinite, consider the difference to be 0.
 // Can be helpful in cases where there is a wide range of expected values,
 // such that it's difficult to have a low absolute difference for large expected
 // values and a low relative difference for near-zero expected values.
 pub fn calc_diff_lesser(x: f64, y: f64) -> (f64, bool) {
-    let (diff_abs, sign_change) = calc_diff_abs(x, y);
-    let diff = if diff_abs == 0.0 || diff_abs.is_nan() || diff_abs.is_infinite() {
-        diff_abs
-    } else {
-        let diff_rel = 2.0 * diff_abs / (x.abs() + y.abs());
-        f64::min(diff_abs, diff_rel)
-    };
+    let (mut diff, sign_change) = calc_diff_abs(x, y);
+    if diff != 0.0 && !diff.is_infinite() { // and implicitly not nan
+        let sum_abs = x.abs() + y.abs();
+        if sum_abs > 2.0 {
+            // use relative difference
+            diff *= 2.0 / sum_abs;
+        }
+    }
     (diff, sign_change)
+}
+
+// Return the absolute difference between two values using a cyclic range,
+// for example angles using a preferred range of [0, 360].
+// Any range enforcement adjustments are reported as a sign change.
+// For example (0, 1) is not reported as a sign change for the range [0, 360],
+// but all of the following are: (1, -1) (359, 361) (0, 361) (720, 721)
+pub fn calc_diff_cyclic(x: f64, y: f64, range_min: f64, range_max: f64) -> (f64, bool) {
+    assert!(range_min < range_max, "range_min must be less than range_max");
+    assert!(range_min <= 0.0 && 0.0 <= range_max, "0.0 must fall within [range_min, range_max]");
+    let xmod = cyclic_range(x, range_min, range_max);
+    let ymod = cyclic_range(y, range_min, range_max);
+    let diff1 = if (xmod.is_nan() && !x.is_nan()) || (ymod.is_nan() && !y.is_nan()) {
+        // This can happen if x or y is infinite, and possibly other degenerate cases.
+        (f64::NAN, true)
+    } else {
+        calc_diff_abs(xmod, ymod)
+    };
+    let diff2 = if xmod < ymod {
+        calc_diff_abs(xmod + range_max - range_min, ymod)
+    } else if xmod > ymod {
+        calc_diff_abs(xmod, ymod + range_max - range_min)
+    } else {
+        diff1
+    };
+    if diff2.0 < diff1.0 {
+        (diff2.0, true)
+    } else {
+        (diff1.0, x != xmod || y != ymod || diff1.1)
+    }
+}
+
+// Adjust a value to fall within a specified cyclic range.
+fn cyclic_range(x: f64, range_min: f64, range_max: f64) -> f64 {
+    let span = range_max - range_min;
+    let xmod = x % span;
+    if xmod < range_min {
+        xmod + span
+    } else if xmod > range_max {
+        xmod - span
+    } else {
+        xmod
+    }
 }
 
 // Return true if diff a is "worse" than diff b.
@@ -63,4 +117,82 @@ pub fn help_sign(x: f64) -> String {
     } else {
         "".to_string()
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{calc_diff_abs, calc_diff_rel, calc_diff_lesser, calc_diff_cyclic};
+
+    #[test]
+    fn test_abs() {
+        // Values chosen to be cleanly representable as exact f64
+        assert_eq!(calc_diff_abs(0.0, 0.5), (0.5, false));
+        assert_eq!(calc_diff_abs(10.0, 10.5), (0.5, false));
+        assert_eq!(calc_diff_abs(-0.25, 0.25), (0.5, true));
+        assert_eq!(calc_diff_abs(0.0, 0.0), (0.0, false));
+        assert_eq!(calc_diff_abs(-0.0, 0.0), (0.0, true));
+        assert_eq!(calc_diff_abs(f64::NAN, f64::NAN), (0.0, false));
+        assert_eq!(calc_diff_abs(f64::NAN, -f64::NAN), (0.0, true));
+        let diff = calc_diff_abs(f64::INFINITY, f64::NAN);
+        assert!(diff.0.is_nan() && !diff.1);
+        assert_eq!(calc_diff_abs(f64::INFINITY, f64::INFINITY), (0.0, false));
+        assert_eq!(calc_diff_abs(f64::INFINITY, f64::NEG_INFINITY), (f64::INFINITY, true));
+    }
+
+    #[test]
+    fn test_rel() {
+        // Values chosen to be cleanly representable as exact f64
+        assert_eq!(calc_diff_rel(0.0, 0.5), (2.0, false));
+        assert_eq!(calc_diff_rel(10.0, 10.5), (1.0 / 20.5, false));
+        assert_eq!(calc_diff_rel(-0.25, 0.25), (2.0, true));
+        assert_eq!(calc_diff_rel(0.0, 0.0), (0.0, false));
+        assert_eq!(calc_diff_rel(-0.0, 0.0), (0.0, true));
+        assert_eq!(calc_diff_rel(f64::NAN, f64::NAN), (0.0, false));
+        assert_eq!(calc_diff_rel(f64::NAN, -f64::NAN), (0.0, true));
+        let diff = calc_diff_rel(f64::INFINITY, f64::NAN);
+        assert!(diff.0.is_nan() && !diff.1);
+        assert_eq!(calc_diff_rel(f64::INFINITY, f64::INFINITY), (0.0, false));
+        let diff = calc_diff_rel(f64::INFINITY, f64::NEG_INFINITY);
+        assert!(diff.0.is_nan() && diff.1);
+    }
+
+    #[test]
+    fn test_lesser() {
+        // Values chosen to be cleanly representable as exact f64
+        assert_eq!(calc_diff_lesser(0.0, 0.5), (0.5, false));
+        assert_eq!(calc_diff_lesser(10.0, 10.5), (1.0 / 20.5, false));
+        assert_eq!(calc_diff_lesser(-0.25, 0.25), (0.5, true));
+        assert_eq!(calc_diff_lesser(0.0, 0.0), (0.0, false));
+        assert_eq!(calc_diff_lesser(-0.0, 0.0), (0.0, true));
+        assert_eq!(calc_diff_lesser(f64::NAN, f64::NAN), (0.0, false));
+        assert_eq!(calc_diff_lesser(f64::NAN, -f64::NAN), (0.0, true));
+        let diff = calc_diff_lesser(f64::INFINITY, f64::NAN);
+        assert!(diff.0.is_nan() && !diff.1);
+        assert_eq!(calc_diff_lesser(f64::INFINITY, f64::INFINITY), (0.0, false));
+        assert_eq!(calc_diff_lesser(f64::INFINITY, f64::NEG_INFINITY), (f64::INFINITY, true));
+    }
+
+    #[test]
+    fn test_cyclic() {
+        // Values chosen to be cleanly representable as exact f64
+        assert_eq!(calc_diff_cyclic(0.0, 0.5, -180.0, 180.0), (0.5, false));
+        assert_eq!(calc_diff_cyclic(10.0, 10.5, -180.0, 180.0), (0.5, false));
+        assert_eq!(calc_diff_cyclic(-0.25, 0.25, -180.0, 180.0), (0.5, true));
+        assert_eq!(calc_diff_cyclic(0.0, 0.0, -180.0, 180.0), (0.0, false));
+        assert_eq!(calc_diff_cyclic(-0.0, 0.0, -180.0, 180.0), (0.0, true));
+        assert_eq!(calc_diff_cyclic(f64::NAN, f64::NAN, -180.0, 180.0), (0.0, true));
+        assert_eq!(calc_diff_cyclic(f64::NAN, -f64::NAN, -180.0, 180.0), (0.0, true));
+        let diff = calc_diff_cyclic(f64::INFINITY, f64::NAN, -180.0, 180.0);
+        assert!(diff.0.is_nan() && diff.1);
+        let diff = calc_diff_cyclic(f64::INFINITY, f64::INFINITY, -180.0, 180.0);
+        assert!(diff.0.is_nan() && diff.1);
+        let diff = calc_diff_cyclic(f64::INFINITY, f64::NEG_INFINITY, -180.0, 180.0);
+        assert!(diff.0.is_nan() && diff.1);
+        assert_eq!(calc_diff_cyclic(-180.0, 180.0, -180.0, 180.0), (0.0, true));
+        assert_eq!(calc_diff_cyclic(-179.0, 179.0, -180.0, 180.0), (2.0, true));
+        assert_eq!(calc_diff_cyclic(-179.0, -179.0, -180.0, 180.0), (0.0, false));
+        assert_eq!(calc_diff_cyclic(181.0, 181.0, -180.0, 180.0), (0.0, true));
+        assert_eq!(calc_diff_cyclic(0.0, 721.0, -180.0, 180.0), (1.0, true));
+    }
+
 }
